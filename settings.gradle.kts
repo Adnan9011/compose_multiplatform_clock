d4aa3d7885bcba9dc27830547272f52c46849d37
Adnan9011 <adnan9011@gmail.com>
2024-02-09 21:13:24 +0330

    feat: Init commit

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
+rootProject.name = "CMP_Clock"
+include(":androidApp")
+include(":shared")
