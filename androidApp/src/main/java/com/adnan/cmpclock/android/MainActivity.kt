32868d99b3392ba15b2899ee8aa1f89c19ce5863
Adnan9011 <adnan9011@gmail.com>
2024-02-09 23:31:39 +0330

    feat: Add main screen

--- androidApp/src/main/java/com/adnan/cmpclock/android/MainActivity.kt
+++ androidApp/src/main/java/com/adnan/cmpclock/android/MainActivity.kt
@@ -3,22 +3,13 @@ package com.adnan.cmpclock.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
-import androidx.compose.ui.Modifier
+import com.adnan.cmpclock.MainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-
-                }
-            }
+            MainScreen()
         }
     }
 }
---
+++ shared/src/commonMain/kotlin/com/adnan/cmpclock/MainScreen.kt
@@ -0,0 +1,26 @@
+package com.adnan.cmpclock
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun MainScreen() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .background(Color.White)
+            .fillMaxHeight()
+            .fillMaxWidth()
+    ) {
+        AnalogClock()
+        DigitalClock()
+    }
+}
